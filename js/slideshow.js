8fc02e6863c6d1b7c048125ef8611c52919ab54f
Yukesh Karki <yukeshkarki34@gmail.com>
2026-01-16 18:07:35 +0000

    added linkedin post section

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,8 @@
 let slides = document.querySelectorAll('.slide');
+let nextBtn = document.getElementById('nextBtn');
+let previousBtn = document.getElementById('previousBtn');
 let index = 0;
-let slideTime = 3000; // 2 seconds per slide
+let slideTime = 10000; // 2 seconds per slide
 
 function showSlide(i) {
     slides.forEach(s => s.style.opacity = 0);
@@ -11,6 +13,21 @@ function nextSlide() {
     index = (index + 1) % slides.length;
     showSlide(index);
 }
+
+function previousSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+}
+
+nextBtn.addEventListener('click', () => {
+    // Optional: Add any actions after slide transition
+    nextSlide();
+});
+
+previousBtn.addEventListener('click', () => {
+    // Optional: Add any actions after slide transition
+    previousSlide();
+});
 
 showSlide(index);
 setInterval(nextSlide, slideTime);
